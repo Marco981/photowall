aef2c4ad8951102ccce429340699f190f72c9ab4
marco981 <marco98x2@gmail.com>
2018-02-04 16:39:35 +0000

    started the project

---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+import Title from "./Title"
+
+class Main extends Component {
+    render () {
+        return  <div> 
+                    <Title title={"Photowall"}/>
+                    
+                </div>
+    }
+}
+
+export default Main
