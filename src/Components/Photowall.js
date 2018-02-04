9fb87776fb84c6c529003162944dbb3ac11482bf
marco981 <marco98x2@gmail.com>
2018-02-04 18:35:52 +0000

    finished the setup of the UI for the app

--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React, {Component} from "react"
 import Photo from "./Photo"
 
-function Photowall(props) {
-    return  <div className ="photoGrid"> 
-                    {props.posts.map((post, index) => <Photo key={index} post={post}/>)}
-                </div>
+class Photowall extends Component {
+    render () {
+        return <div className="photo-grid"> 
+                {this.props.posts.map((post,index) => <Photo key={index} post={post}/> )}
+            </div>
+    }
 }
 
 
