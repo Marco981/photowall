0f5a0d6bb96a7bc2240514e06c0b41bc3d8d02f0
marco981 <marco98x2@gmail.com>
2018-02-04 23:05:56 +0000

    Refactored the components in functional components

--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -1,13 +1,13 @@
-import React, {Component} from "react"
+import React from "react"
 import Photo from "./Photo"
 
-class Photowall extends Component {
-    render () {
-        return <div className="photo-grid"> 
-                {this.props.posts.map((post,index) => <Photo key={index} post={post}/> )}
+function Photowall(props) {
+    return <div className="photo-grid"> 
+                {props.posts.map((post,index) => <Photo key={index} post={post}/> )}
             </div>
-    }
 }
+
+
 
 
 
